a3b49a4eaf97d5b44dbbe9474d3c979a30a9419b
worawut <worawut48@hotmail.co.th>
2024-08-08 17:20:37 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="max-w-3xl mx-auto my-10">{children}</div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 export default function Home() {
   return (
-    <div><Button>test</Button></div>
+    <Card>
+        <CardHeader>
+          <CardTitle>วันที่ตรวจบ้าน</CardTitle>
+          <CardDescription>รายละเอียด</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex justify-end">
+            <Dialog>
+              <DialogTrigger>Open</DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </CardContent>
+      </Card>
   );
 }
